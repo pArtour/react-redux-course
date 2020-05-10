9ad75213313760cdc02b12e106a5cbc3e581039c
pArtour <edvardi72@gmail.com>
2020-05-10 13:57:38 +0300

    Learnt nesting and configuration of components

--- components/src/index.js
+++ components/src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
     return (
-        <div>
-            Hi there!
+        <div className="ui container comments">
+            <CommentDetail author="Sam" timeAgo="Today at 4:45 PM" />
+            <CommentDetail author="Alex" timeAgo="Today at 2:00 AM" />
+            <CommentDetail author="Jane" timeAgo="Yesterday at 5:00 PM" />
         </div>
     );
 };
@@ -12,4 +15,5 @@ const App = () => {
 ReactDOM.render(
     <App/>,
     document.querySelector('#root')
-)
+);
+
