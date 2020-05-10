855ec353f4cad28ba598e76e3049b69cb72663a0
pArtour <edvardi72@gmail.com>
2020-05-10 21:47:05 +0300

    Conditionally Rendering Conten

--- seasons-app/src/index.js
+++ seasons-app/src/index.js
@@ -5,20 +5,27 @@ class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { lat: null };
+        this.state = { lat: null, errorMessage: '' };
 
         window.navigator.geolocation.getCurrentPosition(
             position => {
                 this.setState({ lat: position.coords.latitude });
             },
-            error => console.log(error)
+            error => {
+                this.setState({ errorMessage: error.message });
+            }
         );
-    }
+    };
+
     render() {
-        return (
-            <div>Latitude: {this.state.lat}</div>
-        )
-    }
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+        return <div>Loading</div>
+    };
 }
 
 ReactDOM.render(
