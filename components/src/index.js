d55247134727d02df5674eaf0f3c695abdb28154
pArtour <edvardi72@gmail.com>
2020-05-10 15:20:19 +0300

    Component Reuse

--- components/src/index.js
+++ components/src/index.js
@@ -1,29 +1,41 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import CommentDetail from "./CommentDetail";
 import faker from "faker";
+
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail
-                author="Sam"
-                timeAgo="Today at 4:45 PM"
-                commentText="Nice blog post"
-                image={faker.image.avatar()}
-            />
-            <CommentDetail
-                author="Alex"
-                timeAgo="Today at 2:00 AM"
-                commentText="Cool!"
-                image={faker.image.avatar()}
-            />
-            <CommentDetail
-                author="Jane"
-                timeAgo="Yesterday at 5:00 PM"
-                commentText="Thanks, interesting"
-                image={faker.image.avatar()}
-            />
+            <ApprovalCard>
+                <h4>Warning!</h4>
+                <div>Are you sure?</div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Sam"
+                    timeAgo="Today at 4:45 PM"
+                    commentText="Nice blog post"
+                    image={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Alex"
+                    timeAgo="Today at 2:00 AM"
+                    commentText="Cool!"
+                    image={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Jane"
+                    timeAgo="Yesterday at 5:00 PM"
+                    commentText="Thanks, interesting"
+                    image={faker.image.avatar()}
+                />
+            </ApprovalCard>
         </div>
     );
 };
