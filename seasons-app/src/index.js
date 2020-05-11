bdfec4d7450c0c7b738cd170fc1167862498cead
pArtour <edvardi72@gmail.com>
2020-05-11 12:39:26 +0300

    Styling and default props

--- seasons-app/src/index.js
+++ seasons-app/src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
     // constructor(props) {
@@ -8,7 +10,7 @@ class App extends React.Component {
     //     // this.state = { lat: null, errorMessage: '' };
     // };
 
-    state = { lat: null, errorMessage: ''};
+    state = { lat: null, errorMessage: '' };
 
     componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
@@ -22,9 +24,9 @@ class App extends React.Component {
             return <div>Error: {this.state.errorMessage}</div>
         }
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat} />
         }
-        return <div>Loading</div>
+        return <Spinner message="Pleace accept location request"/>
     };
 }
 
